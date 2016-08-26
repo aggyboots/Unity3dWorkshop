13d37dba83b539d722d2c11ee935446f7c66f994
aggyboots <dunsfoldclench@aol.co.uk>
2016-08-26 09:03:53 +0100

    the wight maze of swag

---
+++ Assets/YourComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YourComponent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.LogFormat ("Time={0}", Time.time);
+	
+	}
+}
